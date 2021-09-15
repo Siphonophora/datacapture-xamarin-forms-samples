afb1258ecc546cc166842f99143468366dd1164d
Michael J Conrad <32316111+Siphonophora@users.noreply.github.com>
2021-09-15 11:05:45 -0400

    Feat: Basic BWH demo, excluding secrets from git

---
+++ Secrets/ScanditKeyEmpty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secrets
+{
+
+    public static partial class ScanditKeySecret
+    {
+        //public const string SCANDIT_LICENSE_KEY = "";
+    }
+}
--- forms/MatrixScanBubblesSample/MatrixScanBubblesSample/Models/ScannerModel.cs
+++ forms/MatrixScanBubblesSample/MatrixScanBubblesSample/Models/ScannerModel.cs
@@ -14,7 +14,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using Scandit.DataCapture.Barcode.Capture.Unified;
 using Scandit.DataCapture.Barcode.Data.Unified;
 using Scandit.DataCapture.Barcode.Tracking.Capture.Unified;
 using Scandit.DataCapture.Core.Capture.Unified;
@@ -24,12 +26,7 @@ namespace MatrixScanBubblesSample.Models
 {
     public class ScannerModel
     {
-        // Enter your Scandit License key here.
-        public const string SCANDIT_LICENSE_KEY = "-- ENTER YOUR SCANDIT LICENSE KEY HERE --";
-
         private static readonly Lazy<ScannerModel> instance = new Lazy<ScannerModel>(() => new ScannerModel(), LazyThreadSafetyMode.PublicationOnly);
-
-        public static ScannerModel Instance => instance.Value;
 
         private ScannerModel()
         {
@@ -37,42 +34,51 @@ namespace MatrixScanBubblesSample.Models
             this.CurrentCamera?.ApplySettingsAsync(this.CameraSettings);
 
             // Create data capture context using your license key and set the camera as the frame source.
-            this.DataCaptureContext = DataCaptureContext.ForLicenseKey(SCANDIT_LICENSE_KEY);
+            this.DataCaptureContext = DataCaptureContext.ForLicenseKey(Secrets.ScanditKeySecret.SCANDIT_LICENSE_KEY);
             this.DataCaptureContext.SetFrameSourceAsync(this.CurrentCamera);
 
-            // The barcode tracking process is configured through barcode tracking settings
-            // and are then applied to the barcode tracking instance that manages barcode tracking.
+            // The barcode tracking process is configured through barcode tracking settings and are
+            // then applied to the barcode tracking instance that manages barcode tracking.
             this.BarcodeTrackingSettings = BarcodeTrackingSettings.Create(BarcodeTrackingScenario.A);
 
-            // The settings instance initially has all types of barcodes (symbologies) disabled.
-            // For the purpose of this sample we enable a very generous set of symbologies.
-            // In your own app ensure that you only enable the symbologies that your app requires as
-            // every additional enabled symbology has an impact on processing times.
+            // The settings instance initially has all types of barcodes (symbologies) disabled. For
+            // the purpose of this sample we enable a very generous set of symbologies. In your own
+            // app ensure that you only enable the symbologies that your app requires as every
+            // additional enabled symbology has an impact on processing times.
             HashSet<Symbology> symbologies = new HashSet<Symbology>
             {
-                Symbology.Ean13Upca,
-                Symbology.Ean8,
-                Symbology.Upce,
-                Symbology.Code39,
+                Symbology.DataMatrix,
                 Symbology.Code128
             };
 
             this.BarcodeTrackingSettings.EnableSymbologies(symbologies);
+            SymbologySettings symbologySettings = BarcodeTrackingSettings.GetSymbologySettings(Symbology.Code128);
+
             this.BarcodeTracking = BarcodeTracking.Create(this.DataCaptureContext, this.BarcodeTrackingSettings);
         }
 
+        public static ScannerModel Instance => instance.Value;
+
         #region DataCaptureContext
+
         public DataCaptureContext DataCaptureContext { get; private set; }
-        #endregion
+
+        #endregion DataCaptureContext
 
         #region CamerSettings
+
         public Camera CurrentCamera { get; private set; } = Camera.GetCamera(CameraPosition.WorldFacing);
+
         public CameraSettings CameraSettings { get; } = BarcodeTracking.RecommendedCameraSettings;
-        #endregion
+
+        #endregion CamerSettings
 
         #region BarcodeTracking
+
         public BarcodeTracking BarcodeTracking { get; private set; }
+
         public BarcodeTrackingSettings BarcodeTrackingSettings { get; private set; }
-        #endregion
+
+        #endregion BarcodeTracking
     }
 }
